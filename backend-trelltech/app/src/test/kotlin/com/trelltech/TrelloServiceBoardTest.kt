400acb1771a6b1486b4003ca353766bb2a85ffe0
TheoDeschamps <theo.deschamps@epitech.eu>
2025-03-30 22:12:06 +0200

    [FIX] reset token table

--- backend-trelltech/app/src/test/kotlin/com/trelltech/TrelloServiceBoardTest.kt
+++ backend-trelltech/app/src/test/kotlin/com/trelltech/TrelloServiceBoardTest.kt
@@ -6,8 +6,9 @@ import com.trelltech.services.TrelloService
 import io.mockk.coEvery
 import io.mockk.mockkConstructor
 import kotlinx.coroutines.runBlocking
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -15,19 +16,20 @@ import kotlin.test.assertEquals
 
 class TrelloServiceBoardTest {
 
-    private val service = TrelloService()
+    private lateinit var service: TrelloService
 
     @BeforeEach
     fun setup() {
         Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;", driver = "org.h2.Driver")
         transaction {
-            create(Tokens)
+            SchemaUtils.drop(Tokens)
+            SchemaUtils.create(Tokens)
             Tokens.insert {
                 it[userId] = "testUser"
                 it[token] = "mockToken"
             }
         }
-
+        service = TrelloService()
         mockkConstructor(com.trelltech.config.TrelloClient::class)
     }
 
@@ -45,9 +47,7 @@ class TrelloServiceBoardTest {
             )
         )
 
-        coEvery {
-            anyConstructed<com.trelltech.config.TrelloClient>().getBoards()
-        } returns expectedBoards
+        coEvery { anyConstructed<com.trelltech.config.TrelloClient>().getBoards() } returns expectedBoards
 
         val result = service.getBoards("testUser")
 
