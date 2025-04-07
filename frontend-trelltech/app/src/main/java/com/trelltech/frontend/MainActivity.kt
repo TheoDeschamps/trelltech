74b76b2ed3e39e1718280de59cac107f3e361d15
Emilien <emilien.dewilde@epitech.eu>
2025-04-07 12:24:47 +0200

    add -> some change

--- frontend-trelltech/app/src/main/java/com/trelltech/frontend/MainActivity.kt
+++ frontend-trelltech/app/src/main/java/com/trelltech/frontend/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    private val getter = Get()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,13 +15,30 @@ class MainActivity : ComponentActivity() {
         val buttonAuth = findViewById<Button>(R.id.buttonAuth)
         val textToken = findViewById<TextView>(R.id.textToken)
         val savedToken = SecurityModule.getToken(this)
+        val board = findViewById<TextView>(R.id.textBoard)
 
         if (!savedToken.isNullOrEmpty()) {
             buttonAuth.visibility = android.view.View.GONE
             textToken.text = "Connected!"
+            getter.getBoards { boards ->
+                runOnUiThread {
+                    board.text = boards.joinToString("\n\n"){ b ->
+                        val keys = b.keys()
+                        val infoList = mutableListOf<String>()
+                        while (keys.hasNext()) {
+                            val key = keys.next()
+                            infoList.add("$key: ${b.get(key)}")
+                        }
+                        infoList.joinToString("\n")
+                    }
+                }
+            }
+
             textToken.setOnClickListener {
                 textToken.text = savedToken
             }
+
+
         } else {
             textToken.text = "Not connected"
             buttonAuth.visibility = android.view.View.VISIBLE
